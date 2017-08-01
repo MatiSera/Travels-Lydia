1ff11375ff2676f4d0ea168ea11c39dfbfbc3c95
“leb90” <leam.90@gmail.com>
2017-08-01 14:18:22 -0300

    change
    change

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 var searchAux;
 $(document).ready(function(){
+    searchAux = document.getElementById("searchButton").style.display = "none";
+})
+//navbar search button
+$(document).ready(function(){
     $("#search").click(function(){
         document.getElementById("searchButton").style.display = "block";
     	document.getElementById("unique-navbar").style.display = "none";
@@ -15,7 +19,7 @@ $(document).ready(function(){
 
 });
 $(document).keyup(function(e) {
-	searchAux = document.getElementById("searchButton").style.display = "none";
+	
 	if (e.keyCode == 27) { // escape key maps to keycode `27`
     	if (searchAux) {
     		document.getElementById("searchButton").style.display = "none";
@@ -23,4 +27,72 @@ $(document).keyup(function(e) {
     	}   
     }
 });
+
+
+function showAllCountries(){
+        $("#all-countries").fadeIn("slow")
+        document.getElementById("eeuu-countries").style.display = "none";
+        document.getElementById("america-countries").style.display = "none";
+        document.getElementById("europe-countries").style.display = "none";
+        document.getElementById("asia-countries").style.display = "none";    
+
+}
+function allCountries(){
+  aux = setTimeout(showAllCountries, 100);
+}
+
+function americaCountries(){
+  aux = setTimeout(showAmericaCountries, 100);
+}
+function europeCountries(){
+  aux = setTimeout(showEuropeCountries, 100);
+}
+function usaCountries(){
+  aux = setTimeout(showUsaCountries, 100);
+}
+function asiaCountries(){
+  aux = setTimeout(showAsiaCountries, 100);
+}
+
+
+
+function showAmericaCountries(){
+    $("#america-countries").fadeIn("slow")
+    document.getElementById("all-countries").style.display = "none";
+    document.getElementById("eeuu-countries").style.display = "none";
+    document.getElementById("europe-countries").style.display = "none";
+    document.getElementById("asia-countries").style.display = "none";
+}
+
+
+
+function showEuropeCountries(){
+    $("#europe-countries").fadeIn("slow")
+    document.getElementById("all-countries").style.display = "none";
+    document.getElementById("eeuu-countries").style.display = "none";
+    document.getElementById("america-countries").style.display = "none";
+    document.getElementById("asia-countries").style.display = "none";
+}
+
+
+
+function showUsaCountries(){
+    $("#eeuu-countries").fadeIn("slow")
+    document.getElementById("all-countries").style.display = "none";
+    document.getElementById("europe-countries").style.display = "none";
+    document.getElementById("america-countries").style.display = "none";
+    document.getElementById("asia-countries").style.display = "none";
+}
+
+
+
+function showAsiaCountries(){
+    $("#asia-countries").fadeIn("slow")
+    document.getElementById("all-countries").style.display = "none";
+    document.getElementById("eeuu-countries").style.display = "none";
+    document.getElementById("america-countries").style.display = "none";
+    document.getElementById("europe-countries").style.display = "none";
+}
+
+
 
