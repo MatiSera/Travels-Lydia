831a9ae4699c593d3dd3a5ed278e51e106ed65d3
Matias Serafini <serafinimatias2@gmail.com>
2017-08-07 13:09:18 -0300

    finish uploading all files

--- js/main.js
+++ js/main.js
@@ -95,6 +95,10 @@ function showAsiaCountries(){
     document.getElementById("america-countries").style.display = "none";
     document.getElementById("europe-countries").style.display = "none";
 }
-
+$(document).ready(function () {
+          if (!$.browser.webkit) {
+              $('.wrapper').html('<p>Sorry! Non webkit users. :(</p>');
+          }
+      });
 
 
